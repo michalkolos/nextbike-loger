d38e6f422c6b2a7e1585b6e9952d4ac385711e91
michalkolos <cool.kolos@gmail.com>
2020-02-29 13:47:45 +0100

    Initial commit Setting up the project.

---
+++ src/main/java/com/michalkolos/nextbikeloger/NextbikeLogerApplication.java
@@ -0,0 +1,13 @@
+package com.michalkolos.nextbikeloger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextbikeLogerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextbikeLogerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michalkolos/nextbikeloger/NextbikeLogerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalkolos.nextbikeloger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextbikeLogerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
